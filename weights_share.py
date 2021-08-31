7a14f4cdbac99d4062bfe5fc1704f2e11029e855
Breenda Das <breenda.das@gmail.com>
2021-09-01 02:58:55 +0530

    Update weights_share.py

--- weights_share.py
+++ weights_share.py
@@ -1,23 +1,32 @@
 import torch
 import numpy as np
 from sklearn.cluster import KMeans
-from scipy.sparse import csc_matrix, csr_matrix
+from scipy.sparse import csc_matrix, csr_matrix, coo_matrix
 
 
-def apply_weight_sharing(model, bits=5):
+def apply_weight_sharing(model):
     """
     Applies weight sharing to the given model
     """
-    for module in model.children():
-        dev = module.weight.device
-        weight = module.weight.data.cpu().numpy()
-        shape = weight.shape
-        mat = csr_matrix(weight) if shape[0] < shape[1] else csc_matrix(weight)
-        min_ = min(mat.data)
-        max_ = max(mat.data)
-        space = np.linspace(min_, max_, num=2**bits)
-        kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1,1), n_init=1, precompute_distances=True, algorithm="full")
-        kmeans.fit(mat.data.reshape(-1,1))
-        new_weight = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
-        mat.data = new_weight
-        module.weight.data = torch.from_numpy(mat.toarray()).to(dev)
+
+    for name, module in model.named_modules():
+        if type(module).__name__ == "Conv2d":
+            dev = module.weight.device
+            c_weight = module.weight.data.cpu().numpy()
+            print(name)
+            for ci, i in enumerate(c_weight):
+                for cj, j in enumerate(i):
+                    try:
+                        shape = j.shape
+                        mat = csr_matrix(j) if shape[0] < shape[1] else csc_matrix(j)
+                        min_ = min(mat.data)
+                        max_ = max(mat.data)
+                        space = np.linspace(min_, max_, num=5)
+                        kmeans = KMeans(n_clusters=len(space), init=space.reshape(-1,1), n_init=1, precompute_distances=True, algorithm="full")
+                        kmeans.fit(mat.data.reshape(-1,1))
+                        new_weight = kmeans.cluster_centers_[kmeans.labels_].reshape(-1)
+                        mat.data = new_weight
+                        module.weight.data[ci][cj] = torch.from_numpy(mat.toarray()).to(dev)
+                    except ValueError:
+                        continue
+    
