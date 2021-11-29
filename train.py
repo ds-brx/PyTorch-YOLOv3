a3bb8286acf0ee61c8ad996f02d8e0e115f2a152
Breenda Das <breenda.das@gmail.com>
2021-11-29 13:24:57 +0530

    Update train.py

--- train.py
+++ train.py
@@ -10,16 +10,15 @@ if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="train pruned model")
     parser.add_argument("-m", "--model", type=str, default="custom_pruned_models/cluster_prune_10.pth", help="Path to model")
+    parser.add_argument("-e", '--epochs', type = int, default=100, help='epochs')
     args = parser.parse_args()
 
     model = torch.load(args.model)
-    model = train.run(model)
+    model = train.run(model, epochs=args.epochs)
 
 
     test.run(model)
 
     path_name = args.model.split('/')[-1].split('.')[0]
-    torch.save(model, 'trained_pruned_models/{}.pth'.format(path_name))
-
-
+    torch.save(model, 'trained_pruned_models/{}_{}.pth'.format(path_name, args.epochs))
 
