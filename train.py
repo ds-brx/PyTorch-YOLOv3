2e772d05bbceee26838b297908c295469c84d446
Breenda Das <breenda.das@gmail.com>
2021-11-29 12:46:38 +0530

    Update train.py

--- train.py
+++ train.py
@@ -1,17 +1,25 @@
-from pytorchyolo.train import run
+from pytorchyolo import train, test
 import os
 import argparse
 import torch
 
-if not os.path.isdir('trained_pruned_models'):
-    os.makedirs('trained_pruned_models')
+if __name__ == "__main__":
 
-parser = argparse.ArgumentParser(description="train pruned model")
-parser.add_argument("-m", "--model", type=str, default="custom_pruned_models", help="Path to model")
-args = parser.parse_args()
+    if not os.path.isdir('trained_pruned_models'):
+        os.makedirs('trained_pruned_models')
 
-model = torch.load(args.model)
-model = run(model)
+    parser = argparse.ArgumentParser(description="train pruned model")
+    parser.add_argument("-m", "--model", type=str, default="custom_pruned_models/cluster_prune_10.pth", help="Path to model")
+    args = parser.parse_args()
+
+    model = torch.load(args.model)
+    model = train.run(model)
+
+
+    test.run(model)
+
+    path_name = args.model.split('/')[-1].split('.')[0]
+    torch.save(model, 'trained_pruned_models/{}.pth'.format(path_name))
 
 
 
